fb2fb7d288cab8e7887291e025b48ebbc40607ae
mash1408 <s.mashayikh@novasemita.org>
2021-03-23 16:44:52 +0530

    player X completed

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // Function called whenever user tab on any box 
-var board=[2,2,2,2,2,2,2,2,2]
+const board=[8,3,4,1,5,9,6,7,2]
+var x=[]
+var o=[]
 var turn=1
 userFlag=0
-let x
+let first
 function checkBox(){
- x=document.getElementById("playerChoice").checked
- console.log(x)
+ first=document.getElementById("playerChoice").checked
+ console.log(first)
 }
 // Function to reset game 
 function myfunc_2() { 
@@ -23,11 +25,11 @@ function myfunc_2() {
 } 
 function make2(){
     if(board[4]=== 2)
-    return 4
+    return 5
     else {
        var random=Math.floor(Math.random() * 4)
-       var rand=[1,3,5,7]
-       while(board[rand[random]]!=2){
+       var rand=[3,1,7,9]
+       while(!checkBlank(rand[random])){
         console.log('make2')
         random=Math.floor(Math.random() * 4)
        }
@@ -36,142 +38,67 @@ function make2(){
         return rand[random]
     }
 }
-function mul(a,b,c){
-    return a*b*c
+function abc(x,y){
+    var k=Math.abs(15-(x+y))
+    if(!board.includes(k))
+        return
+    if(x+y >15){
+        return 0
+    }
+    else if(x+y<15){
+        if(checkBlank(k)){
+            return k
+        }
+        else 
+        return 0
+    }
 }
-function posswin(p){
+function posswin(c){
+let t
+    if(c==='x')
+        t=[...x]
+    else
+        t=[...o]
+for(var i=0;i<t.length;i++){
+    for(var j=i+1;j<t.length;j++){
+        if(abc(t[i],t[j]))
+            return abc(t[i],t[j])
+    }
 
-	var i
-	for(i=0;i<9;i++)
-	{
-		if(board[i]===2)
-		{
-			//row1 - 012
-if(mul(board[0],board[1],board[2])===p*p*2)
-{
-	for(i=0;i<3;i++)
-	{
-		if(board[i]==2)
-			return i;
-	}
-}
-//row2 - 345
-if(mul(board[3],board[4],board[5])===p*p*2)
-{
-	for(i=3;i<6;i++)
-	{
-		if(board[i]==2)
-			return i;
-	}
-}
-//row3 - 678
-if(mul(board[6],board[7],board[8])===p*p*2)
-{
-	for(i=6;i<9;i++)
-	{
-		if(board[i]==2)
-			return i;
-	}
-}
-//col1 - 036
-if(mul(board[0],board[3],board[6])===p*p*2)
-{
-	for(i=0;i<7;i+=3)
-	{
-		if(board[i]==2)
-			return i;
-	}
-}
-//col2 - 147
-if(mul(board[1],board[4],board[7])===p*p*2)
-{
-	for(i=1;i<8;i+=3)
-	{
-		if(board[i]==2)
-			return i;
-	}
-}
-//col3 - 258
-if(mul(board[2],board[5],board[8])===p*p*2)
-{
-	for(i=2;i<9;i+=3)
-	{
-		if(board[i]==2)
-			return i;
-	}
-}
-//d1 - 048
-if(mul(board[0],board[4],board[8])===p*p*2)
-{
-	for(i=0;i<9;i+=4)
-	{
-		if(board[i]==2)
-			return i;
-	}
-}
-//d2 - 246
-if(mul(board[2],board[4],board[6])===p*p*2)
-{
-	for(i=2;i<7;i+=4)
-	{
-		if(board[i]==2)
-			return i;
-	}
-}
-		}
-
-	}
-
-	return 0;
 
 }
-function checkwin(p){
-    if(board[0]==p && board[1]==p && board[2]==p)
-{
-	return p;
-}
-//row2 - 345
-if(board[3]==p && board[4]==p && board[5]==p)
-{
-	return p;
-}
-//row3 - 678
-if(board[6]==p && board[7]==p && board[8]==p)
-{
-	return p;
-}
-//col1 - 036
-if(board[0]==p && board[3]==p && board[6]==p)
-{
-	return p;
-}
-//col2 - 147
-if(board[1]==p && board[4]==p && board[7]==p)
-{
-	return p;
-}
-//col3 - 258
-if(board[2]==p && board[5]==p && board[8]==p)
-{
-	return p;
-}
-//d1 - 048
-if(board[0]==p && board[4]==p && board[8]==p)
-{
-	return p;
-}
-//d2 - 246
-if(board[2]==p && board[4]==p && board[6]==p)
-{
-	return p;
+  return 0  
 }
 
+function checkwin(c){
+    let t
+    if(c==='x')
+        t=[...x]
+    else
+        t=[...o]
+
+    if(t.includes(board[0])&& t.includes(board[1])&&t.includes(board[2]))
+        return 1
+    else if(t.includes(board[3])&& t.includes(board[4])&&t.includes(board[5]))
+        return 1
+    else if(t.includes(board[6])&& t.includes(board[7])&&t.includes(board[8]))
+        return 1
+    else if(t.includes(board[0])&& t.includes(board[3])&&t.includes(board[6]))
+        return 1
+    else if(t.includes(board[1])&& t.includes(board[4])&&t.includes(board[7]))
+        return 1
+    else if(t.includes(board[2])&& t.includes(board[5])&&t.includes(board[8]))
+        return 1
+    else if(t.includes(board[0])&& t.includes(board[4])&&t.includes(board[8]))
+        return 1
+    else if(t.includes(board[2])&& t.includes(board[4])&&t.includes(board[6]))
+        return 1
 	return 0;
 }
 
 function play(){
  
-    if(x){
+    if(first){
         playO()
         refreshBoard()
         userFlag=1;
@@ -185,20 +112,19 @@ function play(){
 }
 function refreshBoard(){
     let boardChars=[]
-    
     for(var i=0;i<9;i++){
-        if(board[i]==3){
-        boardChars[i]="X"
-        }
-        else if(board[i]==5){
-        boardChars[i]="O"
-        }
-        else {
-        boardChars[i]=" "
+        if(checkBlank(board[i]))
+            boardChars[i]=""
+        else{
+            if(x.includes(board[i]))
+                boardChars[i]='x'
+            else
+                boardChars[i]='O'
         }
     }
-    console.log(board)
-    console.log(boardChars)
+console.log(o)
+console.log(x)
+console.log(boardChars)
 document.getElementById("b1").value=boardChars[0]
 document.getElementById("b2").value=boardChars[1]
 document.getElementById("b3").value=boardChars[2]
@@ -213,45 +139,45 @@ document.getElementById("b9").value=boardChars[8]
 function playX(){
     console.log(turn)
     if(turn===1){
-    Go(1)
+    Go(8)
     }
     if(turn === 3){
-        if(board[8]===2){
-            Go(9)
+        if(checkBlank(2)){
+            Go(2)
         }
         else{
-        Go(3)
+        Go(4)
         }
     }
     if (turn === 5){
-        if(posswin(3)!=0){
-            Go(posswin(3)+1)
+        if(posswin('x')!=0){
+            Go(posswin('x'))
         }
-        else if(posswin(5)!=0){
-            Go(posswin(5)+1)
+        else if(posswin('o')!=0){
+            Go(posswin('o'))
 
         }
-        else if(board[6]===2){
-            Go(7)
+        else if(checkBlank(6)){
+            Go(6)
         }
         else{
-            Go(3)
+            Go(4)
         }
     }
 
     if(turn===7 || turn === 9){
-        if(posswin(3)!=0){
-            Go(posswin(3)+1)
+        if(posswin('x')!=0){
+            Go(posswin('x'))
         }
-        else if(posswin(5)!=0){
-            Go(posswin(5)+1)
+        else if(posswin('o')!=0){
+            Go(posswin('o'))
         }
         else{
             Go(anywhereBlank())
         }
     }
-
-    if(checkwin(3)===0)
+    console.log('checkwin:',checkwin('x'))
+    if(checkwin('x')===0)
     return
     else{
     window.alert("MACHINE WON");
@@ -268,53 +194,57 @@ else
     Go(1)
 }
 if(turn === 4){
-    if(posswin(3)!=0)    
-        Go(posswin(3)+1)
+    if(posswin('x')!=0)    
+        Go(posswin('x')+1)
     else
         Go(make2()+1)
     }
 if(turn=== 6){
-    if(posswin(5)!=0)
-     Go(posswin(5)+1)
-    else if(posswin(3)!=0)
-     Go(posswin(3)+1)
+    if(posswin('o')!=0)
+     Go(posswin('o')+1)
+    else if(posswin('x')!=0)
+     Go(posswin('x')+1)
     else
      Go(anywhereBlank()+1)
 }
 if( turn === 8){
-    if(posswin(5)!=0)
-     Go(posswin(5)+1)
-    else if(posswin(3)!=0)
-     Go(posswin(3)+1)
+    if(posswin('o')!=0)
+     Go(posswin('o')+1)
+    else if(posswin('x')!=0)
+     Go(posswin('x')+1)
     else
      Go(anywhereBlank()+1)
     
 }
 
-if(checkwin(5)===0)
+if(checkwin('o')===0)
     return
     else{
     window.alert("MACHINE WON");
     myfunc_2()
     }
 }
-
+function checkBlank(n){
+    if(x.includes(n))
+        return false
+    else if(o.includes(n))
+        return false
+    return true
+}
 function anywhereBlank(){
     var random=Math.floor(Math.random() * 9)
-    var rand=[0,1,2,3,4,5,6,7,8]
-    while(board[rand[random]]!=2)
+    while(!checkBlank(board[random]))
      random=Math.floor(Math.random() * 9)
-     
-     console.log('anywhereblank')
-     console.log(rand[random])
-     return rand[random] 
+     console.log('goanywhere')
+     console.log(board[random])
+     return board[random] 
 }
 function Go(n){
     if(turn%2==1){
-        board[n-1]=3;
+        x.push(n)
     }
     else {
-        board[n-1]=5;
+        o.push(n)
     }
     turn++;
 }
@@ -324,36 +254,36 @@ function user(Id){
 
         if(Id==="b1"){
            
-            Go(1)
+            Go(8)
         }
         else if(Id==="b2"){
         
-            Go(2)
-        }
-        else if(Id==="b3"){
             Go(3)
         }
-        else if(Id==="b4"){
+        else if(Id==="b3"){
             Go(4)
+        }
+        else if(Id==="b4"){
+            Go(1)
         }
         else if(Id==="b5"){
             Go(5)
         }
         else if(Id==="b6"){
-            Go(6)
+            Go(9)
         }
         else if(Id==="b7"){
-            Go(7)
+            Go(6)
         }
         else if(Id==="b8"){
-            Go(8)
+            Go(7)
         }
         else if(Id==="b9"){
-            Go(9)
+            Go(2)
         }
     refreshBoard()
     userFlag=0
-    if(checkwin(3)===0 && turn%2===0 || checkwin(5)===0 && turn%2===1)//turn is incremented above when Go method is implemented
+    if(checkwin('x')===0 && turn%2===0 || checkwin('o')===0 && turn%2===1)//turn is incremented above when Go method is implemented
     play()
     else{
     window.alert("USER WON")
